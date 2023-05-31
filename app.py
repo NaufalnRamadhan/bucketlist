d0338368eb7165effe1a8e1e64612cdba5e83cf4
NaufalnRamadhan <naufalramadhanlubis@gmail.com>
2023-05-31 19:28:40 +0800

    Fist Commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv (dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_recieve = request.form['bucket_give']
+    count = db.bucket.count_documents({})
+    num = count + 1
+    doc = {
+        'num': num,
+        'bucket': bucket_recieve,
+        'done': 0
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': 'data saved'})
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_recieve = request.form['num_give']
+    db.bucket.update_one(
+        { 'num': int(num_recieve) },
+        { '$set': { 'done': 1}}
+    )
+    return jsonify({'msg': 'update done!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    return jsonify({'bucket': bucket_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
